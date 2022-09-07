871859636eec5fd8492ae4d890dac5dd12cec41d
Дмитрий Елфимов <dmitriy.elfimov70@gmail.com>
2022-09-07 22:46:18 +0300

    Добавьте файлы проекта.

---
+++ PCDestroyer/PCDestroyer.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>      //For I/O Functions
+#include <windows.h>   //For Beep & Sleep functions
+#include<stdlib.h>  //For System Pause
+using namespace std;
+
+int main()
+{
+    std::cout << "Ass Destroyer!\n";
+    for (int i = 0; i < 50; i++)
+        Beep(400 + i * 50, i * 40 - i); Sleep(100);
+}
